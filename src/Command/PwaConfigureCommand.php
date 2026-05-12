2d91ca30a43b67dea5933deab22ffc7860b1e9cd
tacman <tacman@users.noreply.github.com>
2026-05-12 14:09:05 +0000

    refactor all commands to remove execute(), new contracts, etc.

--- src/Command/PwaConfigureCommand.php
+++ src/Command/PwaConfigureCommand.php
@@ -1,56 +1,46 @@
 <?php
 
-// use https://www.rebasedata.com/upload?outputFormat=sqlite&referer=/convert-mysql-to-sqlite-online to convert from downloaded mysql sql zip file
+declare(strict_types=1);
+
 namespace Survos\PwaExtraBundle\Command;
 
-use Survos\PhenxBundle\Services\ImportService;
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
-#[AsCommand(
-    name: 'pwa:configure',
-    description: 'create pwa.yaml from composer.json, etc.',
-)]
-class PwaConfigureCommand extends Command
+#[AsCommand('pwa:configure', 'Create pwa.yaml from composer.json, etc.')]
+final class PwaConfigureCommand
 {
-
-    public function __construct(private ParameterBagInterface $bag)
+    public function __construct(private readonly ParameterBagInterface $bag)
     {
-        parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('name', InputArgument::OPTIONAL, 'application name', null)
-            ->addOption('force', null, InputOption::VALUE_NONE, 'Override')
-        ;
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        // get some defaults, like composer.json keys
+    public function __invoke(
+        SymfonyStyle $io,
+        #[Argument('Application name')] ?string $name = null,
+        #[Option('Override existing configuration')] bool $force = false,
+    ): int {
         $projectDir = $this->bag->get('kernel.project_dir');
         $composerFilename = $projectDir . '/composer.json';
+
         if (!file_exists($composerFilename)) {
-            $output->writeln("Missing composer.json!");
-            return self::FAILURE;
+            $io->writeln('Missing composer.json!');
+            return Command::FAILURE;
         }
+
         $composerData = json_decode(file_get_contents($composerFilename), true);
-        foreach (['name','description'] as $requiredField) {
+        foreach (['name', 'description'] as $requiredField) {
             if (!array_key_exists($requiredField, $composerData)) {
-                $output->writeln("Missing $requiredField in composer.json, please add it with ");
-                $output->writeln("composer config $requiredField ");
-                return self::FAILURE;
+                $io->writeln("Missing $requiredField in composer.json, please add it with ");
+                $io->writeln("composer config $requiredField ");
+                return Command::FAILURE;
             }
         }
 
-        $output->writeln("<info>Done</info>");
-        return self::SUCCESS;
+        $io->writeln('<info>Done</info>');
+        return Command::SUCCESS;
     }
 }
